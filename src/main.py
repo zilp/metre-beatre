a99a4bb2410a9ef458be9fa38b75c85433a39f78
imzzheng <imzzheng@yahoo.com>
2016-04-22 23:52:26 -0400

    read whole poem now

--- src/main.py
+++ src/main.py
@@ -109,77 +109,81 @@ def amphibrachEstimate(syllable, place, value):
     return result
 
 
-def main():
+def analyzeMeter(poem):
     pronDict = nltk.corpus.cmudict.dict()
-    reg = re.compile('[^a-zA-Z\']')
-    s = raw_input("Enter a string to analyze: ")
-    meterstress = ""
-    twoSyCount = 0
-    threeSyCount = 0
-    sumvalue = 0
     type = {"iamb": 0, "trochee": 0, "spondee": 0, "pyrrhic": 0,
             "anapest": 0, "dactyl": 0, "amphibrach": 0}
-    for w in s.split():
-        w = re.sub(reg, "", w)
-        w = w.lower()
-        if w in pronDict:
-            pronparse = pronDict[w]
-            if len(pronparse) == 1:
-                teststr = "".join(pronparse[0])
-                temp = "".join(re.findall(r'\d+', teststr))
-                temp = re.sub("2", "1", temp)
-                meterstress += temp
-                type["iamb"] += iambEstimate(temp, twoSyCount, 4)
-                type["trochee"] += trocheeEstimate(temp, twoSyCount, 4)
-                type["spondee"] += spondeeEstimate(temp, twoSyCount, 4)
-                type["pyrrhic"] += pyrrhicEstimate(temp, twoSyCount, 4)
-                type["anapest"] += anapestEstimate(temp, threeSyCount, 4)
-                type["dactyl"] += dactylEstimate(temp, threeSyCount, 4)
-                type["amphibrach"] += amphibrachEstimate(temp, threeSyCount, 4)
-                twoSyCount = (twoSyCount + len(temp)) % 2
-                threeSyCount = (threeSyCount + len(temp)) % 3
-                sumvalue += 4 * len(temp)
-                print(twoSyCount, threeSyCount, sumvalue, temp)
-            else:
-                # need to figure out what to do for multi pronouncations
-                meterstress += '('
-                count = 1
-                for word in pronparse:
-                    teststr = "".join(word)
+    sumvalue = 0
+    reg = re.compile('[^a-zA-Z\']')
+    for s in poem:
+        meterstress = ""
+        twoSyCount = 0
+        threeSyCount = 0
+        for w in s.split():
+            w = re.sub(reg, "", w)
+            w = w.lower()
+            if w in pronDict:
+                pronparse = pronDict[w]
+                if len(pronparse) == 1:
+                    teststr = "".join(pronparse[0])
                     temp = "".join(re.findall(r'\d+', teststr))
                     temp = re.sub("2", "1", temp)
-                    type["iamb"] += iambEstimate(temp, twoSyCount, 1)
-                    type["trochee"] += trocheeEstimate(temp, twoSyCount, 1)
-                    type["spondee"] += spondeeEstimate(temp, twoSyCount, 1)
-                    type["pyrrhic"] += pyrrhicEstimate(temp, twoSyCount, 1)
-                    type["anapest"] += anapestEstimate(temp, threeSyCount, 1)
-                    type["dactyl"] += dactylEstimate(temp, threeSyCount, 1)
-                    type["amphibrach"] += amphibrachEstimate(temp, threeSyCount, 1)
-                    if count < len(pronparse):
-                        count += 1
-                        meterstress += temp + '|'
-                    else:
-                        count = 1
-                        meterstress += temp + ')'
-                teststr = "".join(pronparse[0])
-                temp = "".join(re.findall(r'\d+', teststr))
-                twoSyCount = (twoSyCount + len(temp)) % 2
-                threeSyCount = (threeSyCount + len(temp)) % 3
-                sumvalue += 1 * len(temp)
-        else:
-            # need to figure out if word is not in cmudict
-            print("Cant find", w)
+                    meterstress += temp
+                    type["iamb"] += iambEstimate(temp, twoSyCount, 4)
+                    type["trochee"] += trocheeEstimate(temp, twoSyCount, 4)
+                    type["spondee"] += spondeeEstimate(temp, twoSyCount, 4)
+                    type["pyrrhic"] += pyrrhicEstimate(temp, twoSyCount, 4)
+                    type["anapest"] += anapestEstimate(temp, threeSyCount, 4)
+                    type["dactyl"] += dactylEstimate(temp, threeSyCount, 4)
+                    type[
+                        "amphibrach"] += amphibrachEstimate(temp, threeSyCount, 4)
+                    twoSyCount = (twoSyCount + len(temp)) % 2
+                    threeSyCount = (threeSyCount + len(temp)) % 3
+                    sumvalue += 4 * len(temp)
+                else:
+                    # need to figure out what to do for multi pronouncations
+                    meterstress += '('
+                    count = 1
+                    for word in pronparse:
+                        teststr = "".join(word)
+                        temp = "".join(re.findall(r'\d+', teststr))
+                        temp = re.sub("2", "1", temp)
+                        type["iamb"] += iambEstimate(temp, twoSyCount, 1)
+                        type["trochee"] += trocheeEstimate(temp, twoSyCount, 1)
+                        type["spondee"] += spondeeEstimate(temp, twoSyCount, 1)
+                        type["pyrrhic"] += pyrrhicEstimate(temp, twoSyCount, 1)
+                        type[
+                            "anapest"] += anapestEstimate(temp, threeSyCount, 1)
+                        type["dactyl"] += dactylEstimate(temp, threeSyCount, 1)
+                        type[
+                            "amphibrach"] += amphibrachEstimate(temp, threeSyCount, 1)
+                        if count < len(pronparse):
+                            count += 1
+                            meterstress += temp + '|'
+                        else:
+                            count = 1
+                            meterstress += temp + ')'
+                    teststr = "".join(pronparse[0])
+                    temp = "".join(re.findall(r'\d+', teststr))
+                    twoSyCount = (twoSyCount + len(temp)) % 2
+                    threeSyCount = (threeSyCount + len(temp)) % 3
+                    sumvalue += 1 * len(temp)
+            else:
+                # need to figure out if word is not in cmudict
+                print("Cant find", w)
 
-    #meterstress = simpleCleanup(meterstress)
-    print(meterstress)
+        meterstress = simpleCleanup(meterstress)
+        print(meterstress)
     print("Likelyhood to be iamb is", type["iamb"] / sumvalue)
     print("Likelyhood to be trochee is", type["trochee"] / sumvalue)
-    print("Likelyhood to be spondee is", type["spondee"] / sumvalue)
-    print("Likelyhood to be pyrrhic is", type["pyrrhic"] / sumvalue)
+    #print("Likelyhood to be spondee is", type["spondee"] / sumvalue)
+    #print("Likelyhood to be pyrrhic is", type["pyrrhic"] / sumvalue)
     print("Likelyhood to be anapest is", type["anapest"] / sumvalue)
     print("Likelyhood to be dactyl is", type["dactyl"] / sumvalue)
     print("Likelyhood to be amphibrach is", type["amphibrach"] / sumvalue)
 
 
 if __name__ == "__main__":
-    main()
+    poem = open('Millay Sonnet 42.txt', 'r')
+    raw_lines = poem.readlines()
+    analyzeMeter(raw_lines)
