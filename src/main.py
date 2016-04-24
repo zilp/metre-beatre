dbbb234b1535ec3085976afd3f117d44cb5129fd
imzzheng <imzzheng@yahoo.com>
2016-04-24 18:17:49 -0400

    less value on simple words and added meter length

--- src/main.py
+++ src/main.py
@@ -113,10 +113,12 @@ def analyzeMeter(poem):
     pronDict = nltk.corpus.cmudict.dict()
     type = {"iamb": 0, "trochee": 0, "spondee": 0, "pyrrhic": 0,
             "anapest": 0, "dactyl": 0, "amphibrach": 0}
+    meterlength = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     sumvalue = 0
     reg = re.compile('[^a-zA-Z\']')
     for s in poem:
         meterstress = ""
+        currentMeter = 0
         twoSyCount = 0
         threeSyCount = 0
         for w in s.split():
@@ -129,17 +131,22 @@ def analyzeMeter(poem):
                     temp = "".join(re.findall(r'\d+', teststr))
                     temp = re.sub("2", "1", temp)
                     meterstress += temp
-                    type["iamb"] += iambEstimate(temp, twoSyCount, 4)
-                    type["trochee"] += trocheeEstimate(temp, twoSyCount, 4)
-                    type["spondee"] += spondeeEstimate(temp, twoSyCount, 4)
-                    type["pyrrhic"] += pyrrhicEstimate(temp, twoSyCount, 4)
-                    type["anapest"] += anapestEstimate(temp, threeSyCount, 4)
-                    type["dactyl"] += dactylEstimate(temp, threeSyCount, 4)
-                    type[
-                        "amphibrach"] += amphibrachEstimate(temp, threeSyCount, 4)
+                    if len(temp) == 1:
+                        type["iamb"] += 2
+                        type["trochee"] += 2
+                        type["anapest"] += 2
+                        type["dactyl"] += 2
+                        type["amphibrach"] += 2
+                    else:
+                        type["iamb"] += iambEstimate(temp, twoSyCount, 4)
+                        type["trochee"] += trocheeEstimate(temp, twoSyCount, 4)
+                        type["anapest"] += anapestEstimate(temp, threeSyCount, 4)
+                        type["dactyl"] += dactylEstimate(temp, threeSyCount, 4)
+                        type["amphibrach"] += amphibrachEstimate(temp, threeSyCount, 4)
                     twoSyCount = (twoSyCount + len(temp)) % 2
                     threeSyCount = (threeSyCount + len(temp)) % 3
                     sumvalue += 4 * len(temp)
+                    currentMeter += len(temp)
                 else:
                     # need to figure out what to do for multi pronouncations
                     meterstress += '('
@@ -150,13 +157,9 @@ def analyzeMeter(poem):
                         temp = re.sub("2", "1", temp)
                         type["iamb"] += iambEstimate(temp, twoSyCount, 1)
                         type["trochee"] += trocheeEstimate(temp, twoSyCount, 1)
-                        type["spondee"] += spondeeEstimate(temp, twoSyCount, 1)
-                        type["pyrrhic"] += pyrrhicEstimate(temp, twoSyCount, 1)
-                        type[
-                            "anapest"] += anapestEstimate(temp, threeSyCount, 1)
+                        type["anapest"] += anapestEstimate(temp, threeSyCount, 1)
                         type["dactyl"] += dactylEstimate(temp, threeSyCount, 1)
-                        type[
-                            "amphibrach"] += amphibrachEstimate(temp, threeSyCount, 1)
+                        type["amphibrach"] += amphibrachEstimate(temp, threeSyCount, 1)
                         if count < len(pronparse):
                             count += 1
                             meterstress += temp + '|'
@@ -168,22 +171,50 @@ def analyzeMeter(poem):
                     twoSyCount = (twoSyCount + len(temp)) % 2
                     threeSyCount = (threeSyCount + len(temp)) % 3
                     sumvalue += 1 * len(temp)
+                    currentMeter += len(temp)
             else:
                 # need to figure out if word is not in cmudict
                 print("Cant find", w)
 
         meterstress = simpleCleanup(meterstress)
         print(meterstress)
+        if currentMeter < 16:
+            meterlength[currentMeter] += 1
+
+    best_fit = ""
+    temp_max = 0
+    beat = 2
+    if(type["iamb"] > temp_max):
+        temp_max = type["iamb"]
+        best_fit = "iambic"
+    if(type["trochee"] > temp_max):
+        temp_max = type["trochee"]
+        best_fit = "trochaic"
+    if(type["anapest"] > temp_max):
+        temp_max = type["anapest"]
+        best_fit = "anapestic"
+        beat = 3
+    if(type["dactyl"] > temp_max):
+        temp_max = type["dactyl"]
+        best_fit = "dactylic"
+        beat = 3
+    if(type["amphibrach"] > temp_max):
+        temp_max = type["amphibrach"]
+        best_fit = "amphibrachic"
+        beat = 3
+
+    lengthtype = ["monometer", "dimeter", "trimeter", "tetrameter", 
+                  "pentameter", "hexameter", "heptameter", "octameter"]
     print("Likelyhood to be iamb is", type["iamb"] / sumvalue)
     print("Likelyhood to be trochee is", type["trochee"] / sumvalue)
-    #print("Likelyhood to be spondee is", type["spondee"] / sumvalue)
-    #print("Likelyhood to be pyrrhic is", type["pyrrhic"] / sumvalue)
     print("Likelyhood to be anapest is", type["anapest"] / sumvalue)
     print("Likelyhood to be dactyl is", type["dactyl"] / sumvalue)
     print("Likelyhood to be amphibrach is", type["amphibrach"] / sumvalue)
 
+    print("Best meter fit is", best_fit, lengthtype[(meterlength.index(max(meterlength))-1)//beat])
+
 
 if __name__ == "__main__":
-    poem = open('Millay Sonnet 42.txt', 'r')
+    poem = open('monarch.txt', 'r')
     raw_lines = poem.readlines()
     analyzeMeter(raw_lines)
