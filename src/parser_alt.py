3eee573f64ce2f86fb7fa0c5ce3a3f4c61d554b2
zilp <shilpaksvn@gmail.com>
2016-04-24 19:00:50 -0400

    works for all inputs

--- src/parser_alt.py
+++ src/parser_alt.py
@@ -8,16 +8,13 @@ def get_text(url):
     if r.status_code == 404:
         raise ValueError("Invalid URL.")
     soup = BeautifulSoup(r.text, "html.parser")
-    poem = soup.find_all(class_="field-item even")
-    poem = str(poem[3])
-    poem = poem.replace('<br/>', '\n')
-    poem = re.sub('<[^<]+?>', '', poem)
+    poem_contents = soup.find_all(property="content:encoded")
+    poem = poem_contents[0].get_text('\n')
+    print poem
     poem_raw = poem.split('\n')
     line_list = []
     for x in poem_raw:
         x = str(x.encode('ascii', 'ignore')).strip()
-        if x.startswith('Source:'):
-            break
         line_list.append(x)
     for x in line_list:
         if x == "":
