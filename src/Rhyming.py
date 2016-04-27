af733ba428fc57bce75c39263e27f03406114d3d
dcstlouis <dcstlouis@gmail.com>
2016-04-26 21:46:53 -0400

    Added code to consider long poems

--- src/Rhyming.py
+++ src/Rhyming.py
@@ -7,64 +7,118 @@ from __future__ import absolute_import, print_function
 import string
 import re
 import nltk
+from numba.tests.test_conversion import addition
 
 
-def rhyme(poem):
-    vowels = [u'AO',u'AA',u'IY',u'UW',u'EH',u'IH',u'UH',u'AH',u'AX',u'AE',\
-              u'EY',u'AY',u'OW',u'AW',u'OY',u'ER',u'AXR',u'EH R',u'UH R',\
-              u'AO R',u'AA R',u'IH R',u'IY R',u'AW R']
+def find_end_in_dict(unknown_word):
     pronDict = nltk.corpus.cmudict.dict()
-    final_syllable_list = [] # list of all the final syllables in the poem in order
-    rhyme_pairs = dict() # dict of the syllables and their representative letters in the rhyme scheme
-    rhyme_scheme = [] # list of the rhymes in the poem in ABBA format
+    ending = ''
+    unknown_word_list = [letter for letter in unknown_word]
+    unknown_word_list.reverse()
+    for letter in unknown_word_list:
+        ending += letter
+        if pronDict[ending] is not None:
+            return pronDict[ending]
 
 
-    for line in poem:
-        line_lowercase = line.lower()
-        line_no_punc = re.sub(r'[^a-zA-Z\s]', '', line_lowercase)  # remove all commas and other punctuation
-        word_list = line_no_punc.split()
-        final_word = word_list[-1]
-        if final_word in pronDict:
-            pron = pronDict[final_word][0] # if multiple pronunciations, pick the second one
+def rhyme_generator():
+    letters = list(string.ascii_uppercase)
+    index = 0
+    while True:
+        letter = letters[index % 26]
+        new_letter = letter
+        if index > 25:
+            for i in range(index // 25):
+                new_letter += letter
+        yield new_letter
+        index += 1
 
-            # extract final syllable: vowel plus optional consonant
-            pron.reverse()
-            for i, sound in enumerate(pron):
-                sound = re.sub(r'\d', "", sound) # remove the nuumeric stress markers
-                if sound in vowels:
-                    if i is 0:
-                        final_syllable_list.append(str(sound))
-                    else:
-                        cons = pron[i-1]
-                        final_syllable_list.append(str(sound) + str(cons))
+
+def find_rhyme_scheme(text):
+    vowels = [u'AO', u'AA', u'IY', u'UW', u'EH', u'IH', u'UH', u'AH', u'AX',
+              u'AE', u'EY', u'AY', u'OW', u'AW', u'OY', u'ER', u'AXR',
+              u'EH R', u'UH R', u'AO R', u'AA R', u'IH R', u'IY R', u'AW R']
+    pronDict = nltk.corpus.cmudict.dict()
+    poem = open(text, 'r')
+    # list of all the final syllables in the poem in order
+    final_syllable_list = []
+    # dict of syllables and their representative letters in the rhyme scheme
+    rhyme_pairs = dict()
+    # list of the rhyme scheme of the poem in ABBA format
+    rhyme_scheme = []
+
+    # Step 1: isolate the last syllable of the last word of every line
+    try:
+        raw_lines = poem.readlines()
+        for line in raw_lines:
+            line_lowercase = line.lower()
+            # remove all dashes first
+            line_no_dash = re.sub(r'[\'\,]', "", line_lowercase)
+            # then remove all commas and other punctuation
+            line_no_punc = re.sub(r'[^a-zA-Z\s]', " ", line_no_dash)
+            word_list = line_no_punc.split()
+            final_word = word_list[-1]
+            if final_word in pronDict:
+                # need to make a copy of pron so it doesn't reverse dict entry
+                pron = []
+                # if multiple pronunciations, pick the first one
+                pron += pronDict[final_word][0]
+                # extract final syllable: vowel plus optional consonant
+                pron.reverse()
+                for i, sound in enumerate(pron):
+                    # remove the numeric stress markers
+                    sound = re.sub(r'\d', "", sound)
+                    if sound in vowels:
+                        if i is 0:
+                            final_syllable_list.append(str(sound))
+                        else:
+                            # need the consonant sound as part of the rhyme
+                            cons = pron[i-1]
+                            final_syllable_list.append(str(sound) + str(cons))
+                        break
+            else:
+                # search for a similar final syllable in pronDict
+                pron = find_end_in_dict(final_word)[0]
+                syllable = ""
+                if pron is not None:
+                    for sound in pron:
+                        # remove the numeric stress markers
+                        sound = re.sub(r'\d', "", sound)
+                        syllable += sound
+                    final_syllable_list.append(syllable)
+                else:
+                    # if no pron in pronDict, signal that with '?'
+                    final_syllable_list.append('?')
                     break
 
-        else:
-            return "Following word not in dictionary: {}".format(final_word)
+    except IOError:
+        return "File not found."
+    finally:
+        poem.close()
 
+    # Step 2. Return rhyme scheme and number of distinct rhymes
 
-    print("Final syllable list is {}".format(final_syllable_list))
-
-    final_syllables = []
+    distinct_rhymes = []  # set of final syllables (no duplicates)
     for syllable in final_syllable_list:
-        if syllable not in final_syllables:
-            final_syllables.append(syllable)
+        if syllable not in distinct_rhymes:
+            distinct_rhymes.append(syllable)
 
-    print("Final syllables are {}".format(final_syllables))
-    num_syllables = len(final_syllables)
+    num_distinct_rhymes = len(distinct_rhymes)  # number of distinct rhymes
 
-    rhymes = string.ascii_uppercase[:num_syllables]
+    # select letters to demonstrate rhyme scheme
+    rhymes = rhyme_generator()
 
-    for syllable, letter in zip(final_syllables, rhymes):
+    # match up a rhyme with a letter
+    for syllable, letter in zip(distinct_rhymes, rhymes):
         if syllable not in rhyme_pairs:
             rhyme_pairs[syllable] = letter
 
     for syllable in final_syllable_list:
-        print(syllable)
         rhyme_scheme.append(rhyme_pairs[syllable])
 
-    return rhyme_scheme
+    print(rhyme_scheme)
+    print("This poem uses {} distinct rhymes.".format(num_distinct_rhymes))
 
 
 if __name__ == '__main__':
-    main()
+    find_rhyme_scheme('Longfellow Hiawatha.txt')
