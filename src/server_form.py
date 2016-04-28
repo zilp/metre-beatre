41c829f14d5d789884b425d38d1aa8bdac556344
zilp <shilpaksvn@gmail.com>
2016-04-27 20:33:31 -0400

    pep8

--- src/server_form.py
+++ src/server_form.py
@@ -4,8 +4,8 @@ Created on Apr. 24, 2016
 @author: shilpa
 '''
 
-#!/usr/bin/python
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+# !/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from os import curdir, sep
 import cgi
 import main
@@ -13,89 +13,90 @@ import main
 PORT_NUMBER = 8080
 
 display_text = '''
-	<html>
-	  <header>
-		  <title>METRE-BEATRE</title>
-			 <style>
-				  body {
-					font-family: Helvetica;
-					font-size: 16px;
-					text-align: center;
-				  }
-				  h1 {
-				  	font-weight: 100;
-				  }
-			</style>
-	  </header>
-		<body>
-		  <h1>METRE-BEATRE <br/> <span style = "font-size: 20px"> guesses a poem's meter + rhyme scheme! </span> </h1>
-			<form method="POST" action="/">
-				<label>Insert URL of poem (on Poets.org or Poetry Foundation):</label> <br/>
-				<input type="text" name="poem"/> <br/>
-				<input type="submit" value="Send"/>
-			</form>
-			<pre>
-			 %s
-			</pre>
-		</body>
-	</html>
-	'''
+    <html>
+      <header>
+          <title>METRE-BEATRE</title>
+             <style>
+                  body {
+                    font-family: Helvetica;
+                    font-size: 16px;
+                    text-align: center;
+                  }
+                  h1 {
+                      font-weight: 100;
+                  }
+            </style>
+      </header>
+        <body>
+          <h1>METRE-BEATRE <br/>
+          <span style = "font-size: 20px">
+          guesses a poem's meter + rhyme scheme!
+          </span> </h1>
+            <form method="POST" action="/">
+                <label>
+                    Insert URL of poem (on Poets.org or Poetry Foundation):
+                </label> <br/>
+                <input type="text" name="poem"/> <br/>
+                <input type="submit" value="Send"/>
+            </form>
+            <pre>
+             %s
+            </pre>
+        </body>
+    </html>
+    '''
+
 
 class myHandler(BaseHTTPRequestHandler):
 
-	#Handler for the GET requests
-	def do_GET(self):
-		if self.path=="/":
-			self.path="/server_form.html"
+    # get request handler
+    def do_GET(self):
+        if self.path=="/":
+            self.path="/server_form.html"
 
-		try:
-			#Check the file extension required and
-			#set the right mime type
+        try:
+            sendReply = False
+            if self.path.endswith(".html"):
+                mimetype='text/html'
+                sendReply = True
+            if sendReply is True:
+                f = open(curdir + sep + self.path)
+                self.send_response(200)
+                self.send_header('Content-type', mimetype)
+                self.end_headers()
+                self.wfile.write(display_text % "")
+                f.close()
+            return
 
-			sendReply = False
-			if self.path.endswith(".html"):
-				mimetype='text/html'
-				sendReply = True
+        except IOError:
+            self.send_error(404, 'File not found: %s' % self.path)
 
-			if sendReply == True:
-				#Open the static file requested and send it
-				f = open(curdir + sep + self.path)
-				self.send_response(200)
-				self.send_header('Content-type',mimetype)
-				self.end_headers()
-				self.wfile.write(display_text % "")
-				f.close()
-			return
+    # handles post requests
+    def do_POST(self):
+        try:
+            if self.path=="/":
+                form = cgi.FieldStorage(
+                    fp=self.rfile,
+                    headers=self.headers,
+                    environ={'REQUEST_METHOD': 'POST',
+                             'CONTENT_TYPE': self.headers['Content-Type'],
+                            })
 
-		except IOError:
-			self.send_error(404,'File not found: %s' % self.path)
+                print "Poem: %s" % form["poem"].value
+                poem = form["poem"].value
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(display_text % main.main(poem))
+                return
 
-	#Handler for the POST requests
-	def do_POST(self):
-		try:
-			if self.path=="/":
-				form = cgi.FieldStorage(
-					fp=self.rfile,
-					headers=self.headers,
-					environ={'REQUEST_METHOD':'POST',
-			                 'CONTENT_TYPE':self.headers['Content-Type'],
-				})
-
-				print "Poem: %s" % form["poem"].value
-				poem = form["poem"].value
-				self.send_response(200)
-				self.end_headers()
-				self.wfile.write(display_text % main.main(poem))
-				return
-
-		except KeyError:
-			self.wfile.write(display_text % "Invalid input.")
+        except KeyError:
+            self.wfile.write(display_text % "Invalid input.")
 
 try:
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port ' , PORT_NUMBER
-	server.serve_forever()
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print 'Started httpserver on port ', PORT_NUMBER
+    server.serve_forever()
 
 except KeyboardInterrupt:
-	print 'Shutting down server...'
-	server.socket.close()
+    print 'Shutting down server...'
+    server.socket.close()
